e0aa2843fe722a69c57eebfd51fc070c8e0f97f7
Paul Friederichsen <floogulinc@gmail.com>
2015-02-16 13:13:39 -0600

    Added DriveTrain subsystem, still need to make it do something.

---
+++ src/com/frc2879/eva/subsystems/DriveTrain.java
@@ -0,0 +1,18 @@
+package com.frc2879.eva.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
